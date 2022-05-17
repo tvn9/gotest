f559ed2e7d138d13f316e909ba762b769fd391ef
Thanh Nguyen <chrisng9@outlook.com>
2022-05-17 03:39:23 -0700

    adding a restclient test app

---
+++ restclient/restclient.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://api.theysaidso.com/god.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(contents))
+}
